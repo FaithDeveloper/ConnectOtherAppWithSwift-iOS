27e44d531c1204191b2336f7d26f1e4faceeb8a5
FaithDeveloper <dev.faithpeople@gmail.com>
2018-01-02 09:26:22 +0900

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+# sampleConnectOtherAppWithSwift
+swift로 다른 앱 연결 샘플 코드
+
+### 1. TestAppConnectClient
+TestAppConnectClient 는 앱을 연결 시도하는 프로젝트입니다.
+
+### 2. TestAppConnectServer
+TestAppConnectServer 는 앱을 연결 하고자 하는 프로젝트입니다.
+
+자세한 내용은 다음을 참고해주세요.
+
+[Go to WebSite](http://faith-developer.tistory.com/entry/Swift3-%EC%95%B1%EA%B3%BC-%EB%8B%A4%EB%A5%B8-%EC%95%B1-%EC%97%B0%EA%B2%B0)
---
+++ TestAppConnectServer/TestAppConnectServer/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TestAppConnectServer
+//
+//  Created by sigong_shin on 2017. 10. 30..
+//  Copyright © 2017년 kcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnSendApp(_ sender: Any) {
+        trueAppInstall(name: "test0001")
+    }
+    
+    //MARK: true App 설치 또는 앱 연결
+    func trueAppInstall (name:String) {
+        if let appUrl = URL(string: "\(name)://") {
+            if UIApplication.shared.canOpenURL(appUrl) {
+                UIApplication.shared.open(appUrl, options: [:], completionHandler: nil)
+            } else {
+                // app store 이동
+                openURLToAppStore(urlPath: name)
+            }
+        }
+    }
+    
+    //MARK : 앱스토어로 이동
+    func openURLToAppStore(urlPath : String){
+        print("url = \(urlPath)")
+        if let url = URL(string: urlPath),
+            UIApplication.shared.canOpenURL(url)
+        {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+}
+
